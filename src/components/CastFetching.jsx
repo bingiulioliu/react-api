d0f8ab8b4028db986344376cbaeb9e7cac61c89f
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-05-07 16:43:56 +0200

    ADD actressess fetch + useEffect

--- src/components/CastFetching.jsx
+++ src/components/CastFetching.jsx
@@ -10,8 +10,9 @@ const API_URL_ACTRESSES = 'https://lanciweb.github.io/demo/api/actresses/';
 function CastFetching() {
 
     const [actors, setActors] = useState([]);
+    const [actresses, setActresses] = useState([]);
 
-
+    // Fetch attori
     const actorsFetch = () => {
         return fetch(API_URL_ACTORS)
             .then(response => response.json())
@@ -42,6 +43,37 @@ function CastFetching() {
 
     console.log(actors);
 
+
+    //Fetch attrici
+    const actressesFetch = () => {
+        return fetch(API_URL_ACTRESSES)
+            .then(response => response.json())
+            .then((jsonActresses) => {
+
+                return jsonActresses.map(item => {
+
+                    return {
+                        genre: 'female',
+                        name: item.name,
+                        birth_year: item.birth_year,
+                        nationality: item.nationality,
+                        known_for: item.known_for,
+                        awards: item.awards,
+                        biography: item.biography,
+                        image: item.image
+                    };
+                });
+            })
+            .catch(err => console.error("Errore nella fetch:", err));
+    };
+
+    useEffect(() => {
+        actressesFetch().then(data => {
+            setActresses(data);
+        });
+    }, []);
+
+    console.log(actresses);
 
 
     return <>
